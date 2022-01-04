c3cb738fdb5fd68485fab38861f01aada75e36e6
Videon <bausn37@gmail.com>
2022-01-04 19:43:41 +0100

    First implementation

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+var points = 260;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
-  background(220);
+  //background(220, 110, 55);
+  //points = noise(mouseX,mouseY)*360;
+stroke(random(0,255),random(0,255),random(0,255));
+  corona();
+}
+
+function corona() {
+  var midPoint = createVector(mouseX-windowWidth/2, mouseY-windowHeight/2);
+  var startPoints = [];
+  var endPoints = [];
+  startPoints.length = points;
+  endPoints.length = points;
+
+  size = 50;
+  size = mouseY/windowHeight*50;
+
+  segrad = 2 * PI / points;
+
+  for (var i = 0; i < startPoints.length; i++) {
+    startPoints[i] = createVector(
+      midPoint.x + cos(segrad * i) * size,
+      midPoint.y + sin(segrad * i) * size
+    );
+
+    endPoints[i] = createVector(
+      midPoint.x + cos(segrad * i) * size * 100,
+      midPoint.y + sin(segrad * i) * size * 100
+    );
+  }
+
+  //Draw lines
+  for (i = 0; i < startPoints.length; i++) {
+    line(startPoints[i].x, startPoints[i].y,
+      endPoints[i].x, endPoints[i].y);
+  }
 }
